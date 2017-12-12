bcb095987bf8133046cf750de81e3fc4bfef8d4a
bluelovers <codelovers@users.sourceforge.net>
2017-12-12 13:30:43 +0800

    rewrite (Step: 9)
    Step: 9
    
    [A ] new file:
    
    	package-lock.json
    	tsconfig.json
    
    [D ] deleted:
    
    	src/js/util/console.ts
    	src/js/util/slugify.ts
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	package.json
    	spec/epub-maker-spec.ts
    	src/js-epub-maker.ts
    	src/js/epub-types.ts
    	src/js/template-builders/idpf-wasteland-builder.ts
    	src/js/template-builders/lightnovel-builder.ts
    	src/js/util/ajax.ts
    	src/js/util/handlebar-helpers.ts

--- src/js/epub-types.ts
+++ src/js/epub-types.ts
@@ -1,115 +1,111 @@
-/* global module */
-(function() {
-   'use strict';
-   
-   // source: http://www.idpf.org/epub/vocab/structure/epub-vocab-structure-20150826.html
-   var epubtypes = [
-      {
-         'name':'abstract',
-         'group': 'Front Matter',
-         'description':'A short summary of the principle ideas, concepts and conclusions of the work, or of a section or except within it.'
-      },
-      {
-         'name':'foreword',
-         'group': 'Front Matter',
-         'description':'An introductory section that precedes the work, typically not written by the work\'s author.'
-      },
-      {
-         'name':'preface',
-         'group': 'Front Matter',
-         'description':'An introductory section that precedes the work, typically written by the work\'s author.'
-      },
-      {
-         'name':'introduction',
-         'group': 'Front Matter',
-         'description':'A section in the beginning of the work, typically introducing the reader to the scope or nature of the work\'s content.'
-      },
-      {
-         'name':'preamble',
-         'group': 'Front Matter',
-         'description':'A section in the beginning of the work, typically containing introductory and/or explanatory prose regarding the scope or nature of the work\'s content'
-      },
-      {
-         'name':'epigraph',
-         'group': 'Front Matter',
-         'description':'A quotation that is pertinent but not integral to the text.'
-      },
-      {
-         'name':'non-specific frontmatter',
-         'group': 'Front Matter',
-         'description': 'Content placed in the frontmatter section, but which has no specific semantic meaning.'
-      },
-      {
-         'name':'part',
-         'group': 'Body Matter',
-         'description':'An introductory section that sets the background to a story, typically part of the narrative.'
-      },
-      {
-         'name':'chapter',
-         'group': 'Body Matter',
-         'description':'An introductory section that sets the background to a story, typically part of the narrative.'
-      },
-      {
-         'name':'prologue',
-         'group': 'Body Matter',
-         'description':'An introductory section that sets the background to a story, typically part of the narrative.'
-      },
-      {
-         'name':'conclusion',
-         'group': 'Body Matter',
-         'description':'An ending section that typically wraps up the work.'
-      },
-      {
-         'name':'epilogue',
-         'group': 'Body Matter',
-         'description':'A concluding section that is typically written from a later point in time than the main story, although still part of the narrative.'
-      },
-      {
-         'name':'afterword',
-         'group': 'Back Matter',
-         'description':'A closing statement from the author or a person of importance to the story, typically providing insight into how the story came to be written, its significance or related events that have transpired since its timeline.'
-      },
-      {
-         'name':'non-specific backmatter',
-         'group': 'Back Matter',
-         'description': 'Content placed in the backmatter section, but which has no specific semantic meaning.'
-      },
-      {
-         'name':'rearnote',
-         'group': 'Back Matter',
-         'description':'A note appearing in the rear (backmatter) of the work, or at the end of a section.'
-      }
-   ];
-   
-   var groups = {};
-   for (var i = 0; i < epubtypes.length; i++) {
-       var group = epubtypes[i].group;
-       (groups[group] || (groups[group] = [])).push(epubtypes[i]);
-   }
-   
-   function getGroup(epubtype) {
-      return {
-         'abstract': 'frontmatter',
-         'foreword': 'frontmatter',
-         'preface': 'frontmatter',
-         'introduction': 'frontmatter',
-         'preamble': 'frontmatter',
-         'epigraph': 'frontmatter',
-         'non-specific frontmatter': 'frontmatter',
-         'part': 'bodymatter',
-         'chapter': 'bodymatter',
-         'prologue': 'bodymatter',
-         'conclusion': 'bodymatter',
-         'epilogue': 'bodymatter',
-         'afterword': 'backmatter',
-         'non-specific backmatter': 'backmatter',
-         'rearnote': 'backmatter'
-      }[epubtype];
-   }
-   
-   module.exports = { 
-      types: epubtypes, 
-      groups: groups,
-      getGroup: getGroup
-   };
-})();
+// source: http://www.idpf.org/epub/vocab/structure/epub-vocab-structure-20150826.html
+let epubtypes = [
+	{
+		'name': 'abstract',
+		'group': 'Front Matter',
+		'description': 'A short summary of the principle ideas, concepts and conclusions of the work, or of a section or except within it.'
+	},
+	{
+		'name': 'foreword',
+		'group': 'Front Matter',
+		'description': 'An introductory section that precedes the work, typically not written by the work\'s author.'
+	},
+	{
+		'name': 'preface',
+		'group': 'Front Matter',
+		'description': 'An introductory section that precedes the work, typically written by the work\'s author.'
+	},
+	{
+		'name': 'introduction',
+		'group': 'Front Matter',
+		'description': 'A section in the beginning of the work, typically introducing the reader to the scope or nature of the work\'s content.'
+	},
+	{
+		'name': 'preamble',
+		'group': 'Front Matter',
+		'description': 'A section in the beginning of the work, typically containing introductory and/or explanatory prose regarding the scope or nature of the work\'s content'
+	},
+	{
+		'name': 'epigraph',
+		'group': 'Front Matter',
+		'description': 'A quotation that is pertinent but not integral to the text.'
+	},
+	{
+		'name': 'non-specific frontmatter',
+		'group': 'Front Matter',
+		'description': 'Content placed in the frontmatter section, but which has no specific semantic meaning.'
+	},
+	{
+		'name': 'part',
+		'group': 'Body Matter',
+		'description': 'An introductory section that sets the background to a story, typically part of the narrative.'
+	},
+	{
+		'name': 'chapter',
+		'group': 'Body Matter',
+		'description': 'An introductory section that sets the background to a story, typically part of the narrative.'
+	},
+	{
+		'name': 'prologue',
+		'group': 'Body Matter',
+		'description': 'An introductory section that sets the background to a story, typically part of the narrative.'
+	},
+	{
+		'name': 'conclusion',
+		'group': 'Body Matter',
+		'description': 'An ending section that typically wraps up the work.'
+	},
+	{
+		'name': 'epilogue',
+		'group': 'Body Matter',
+		'description': 'A concluding section that is typically written from a later point in time than the main story, although still part of the narrative.'
+	},
+	{
+		'name': 'afterword',
+		'group': 'Back Matter',
+		'description': 'A closing statement from the author or a person of importance to the story, typically providing insight into how the story came to be written, its significance or related events that have transpired since its timeline.'
+	},
+	{
+		'name': 'non-specific backmatter',
+		'group': 'Back Matter',
+		'description': 'Content placed in the backmatter section, but which has no specific semantic meaning.'
+	},
+	{
+		'name': 'rearnote',
+		'group': 'Back Matter',
+		'description': 'A note appearing in the rear (backmatter) of the work, or at the end of a section.'
+	}
+];
+
+export let groups = {};
+for (let i = 0; i < epubtypes.length; i++)
+{
+	let group = epubtypes[i].group;
+	(groups[group] || (groups[group] = [])).push(epubtypes[i]);
+}
+
+export function getGroup(epubtype)
+{
+	return {
+		'abstract': 'frontmatter',
+		'foreword': 'frontmatter',
+		'preface': 'frontmatter',
+		'introduction': 'frontmatter',
+		'preamble': 'frontmatter',
+		'epigraph': 'frontmatter',
+		'non-specific frontmatter': 'frontmatter',
+		'part': 'bodymatter',
+		'chapter': 'bodymatter',
+		'prologue': 'bodymatter',
+		'conclusion': 'bodymatter',
+		'epilogue': 'bodymatter',
+		'afterword': 'backmatter',
+		'non-specific backmatter': 'backmatter',
+		'rearnote': 'backmatter'
+	}[epubtype];
+}
+
+export const types = epubtypes;
+
+// @ts-ignore
+export default exports
