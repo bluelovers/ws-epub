8096417aeb2839fa9d4828d95a55a30cb235ef8c
bluelovers <codelovers@users.sourceforge.net>
2017-12-12 13:30:45 +0800

    rewrite (Step: 13)
    Step: 13
    
    [A ] new file:
    
    	package-lock.json
    	tsconfig.json
    
    [D ] deleted:
    
    	src/js/util/console.ts
    	src/js/util/slugify.ts
    
    [ M] modified - Changes not staged for commit:
    
    	.gitignore
    	package.json
    	spec/epub-maker-spec.ts
    	src/js-epub-maker.ts
    	src/js/epub-types.ts
    	src/js/template-builders/idpf-wasteland-builder.ts
    	src/js/template-builders/lightnovel-builder.ts
    	src/js/util/ajax.ts
    	src/js/util/handlebar-helpers.ts

--- src/js/util/handlebar-helpers.ts
+++ src/js/util/handlebar-helpers.ts
@@ -1,25 +1,29 @@
-/* global Handlebars */
-(function() {
-    'use strict';
-    
-    var mimetypes = {
-        'jpeg': 'image/jpeg',
-        'jpg': 'image/jpeg',
-        'bmp': 'image/bmp',
-        'png': 'image/png',
-        'svg': 'image/svg+xml',
-        'gif': 'image/gif'
-    };
+import Handlebars from 'handlebars';
 
-    Handlebars.registerHelper('extension', function(str) {
-        return ext(str);
-    });
-    
-    Handlebars.registerHelper('mimetype', function(str) {
-        return mimetypes[ext(str)];
-    });
-    
-    function ext(str) {
-        return str.substr(str.lastIndexOf('.') + 1);
-    }
-})();
+export const mimetypes = {
+	'jpeg': 'image/jpeg',
+	'jpg': 'image/jpeg',
+	'bmp': 'image/bmp',
+	'png': 'image/png',
+	'svg': 'image/svg+xml',
+	'gif': 'image/gif'
+};
+
+Handlebars.registerHelper('extension', function (str)
+{
+	return ext(str);
+});
+
+Handlebars.registerHelper('mimetype', function (str)
+{
+	return mimetypes[ext(str)];
+});
+
+function ext(str)
+{
+	return str.substr(str.lastIndexOf('.') + 1);
+}
+
+export { Handlebars };
+
+export default Handlebars;
