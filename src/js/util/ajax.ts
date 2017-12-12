d6807f7c10d36ff11f92b2fc7508d0ce1f0f7949
bluelovers <codelovers@users.sourceforge.net>
2017-12-12 15:03:20 +0800

    . (Step: 6)
    Step: 6
    
    [ M] modified - Changes not staged for commit:
    
    	package-lock.json
    	package.json
    	src/js-epub-maker.ts
    	src/js/template-builders/idpf-wasteland-builder.ts
    	src/js/template-builders/lightnovel-builder.ts
    	src/js/util/ajax.ts

--- src/js/util/ajax.ts
+++ src/js/util/ajax.ts
@@ -1,10 +1,24 @@
 namespace xhr
 {
 	// @ts-ignore
-	export let x = (XMLHttpRequest || ActiveXObject || require("xmlhttprequest").XMLHttpRequest);
-}
+	export let x;
 
-export const XMLHttpRequest = xhr.x;
+	// @ts-ignore
+	if ((null, typeof XMLHttpRequest) !== 'undefined')
+	{
+		x = XMLHttpRequest;
+	}
+	// @ts-ignore
+	else if ((null, typeof ActiveXObject) !== 'undefined')
+	{
+		x = ActiveXObject;
+	}
+	else
+	{
+		// @ts-ignore
+		x = require("xmlhttprequest").XMLHttpRequest;
+	}
+}
 
 import * as D from 'd.js';
 
@@ -14,7 +28,7 @@ export function ajax(url, data?): Promise<any>
 
 	try
 	{
-		let x = new (XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
+		let x = new (xhr.x)('MSXML2.XMLHTTP.3.0');
 		x.open(data ? 'POST' : 'GET', url, 1);
 		x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 		x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
