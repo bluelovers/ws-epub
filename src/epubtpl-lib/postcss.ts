aa952acaec2e92f517abab01a52168d40d1e5090
bluelovers <codelovers@users.sourceforge.net>
2017-12-15 15:21:42 +0800

    . (Step: 4)
    Step: 4
    
    [R ] renamed:
    
    	src/epub_templates/lib/ajax.ts -> src/epubtpl-lib/ajax.ts
    	src/epub_templates/lib/handlebar-helpers.ts -> src/epubtpl-lib/handlebar-helpers.ts
    	src/epub_templates/from_idpf_epub3/tpl/mimetype -> src/epubtpl-lib/res/mimetype
    	src/epub_templates/from_idpf_epub3/about_these_templates.txt -> src/epubtpl/from_idpf_epub3/about_these_templates.txt
    	src/epub_templates/from_idpf_epub3/tpl/EPUB/sections-template.xhtml -> src/epubtpl/from_idpf_epub3/tpl/EPUB/sections-template.xhtml
    	src/epub_templates/from_idpf_epub3/tpl/EPUB/wasteland-content.xhtml -> src/epubtpl/from_idpf_epub3/tpl/EPUB/wasteland-content.xhtml
    	src/epub_templates/from_idpf_epub3/tpl/EPUB/wasteland-cover.jpg -> src/epubtpl/from_idpf_epub3/tpl/EPUB/wasteland-cover.jpg
    	src/epub_templates/from_idpf_epub3/tpl/EPUB/wasteland-nav.xhtml -> src/epubtpl/from_idpf_epub3/tpl/EPUB/wasteland-nav.xhtml
    	src/epub_templates/from_idpf_epub3/tpl/EPUB/wasteland.css -> src/epubtpl/from_idpf_epub3/tpl/EPUB/wasteland.css
    	src/epub_templates/from_idpf_epub3/tpl/EPUB/wasteland.ncx -> src/epubtpl/from_idpf_epub3/tpl/EPUB/wasteland.ncx
    	src/epub_templates/from_idpf_epub3/tpl/EPUB/wasteland.opf -> src/epubtpl/from_idpf_epub3/tpl/EPUB/wasteland.opf
    	src/epub_templates/from_idpf_epub3/tpl/META-INF/container.xml -> src/epubtpl/from_idpf_epub3/tpl/META-INF/container.xml
    	src/epub_templates/lightnovel/tpl/mimetype -> src/epubtpl/from_idpf_epub3/tpl/mimetype
    	src/epub_templates/lightnovel/index.ts -> src/epubtpl/lightnovel/index.ts
    	src/epub_templates/lightnovel/tpl/EPUB/auto-toc.html -> src/epubtpl/lightnovel/tpl/EPUB/auto-toc.html
    	src/epub_templates/lightnovel/tpl/EPUB/content.html -> src/epubtpl/lightnovel/tpl/EPUB/content.html
    	src/epub_templates/lightnovel/tpl/EPUB/css/main.css -> src/epubtpl/lightnovel/tpl/EPUB/css/main.css
    	src/epub_templates/lightnovel/tpl/EPUB/lightnovel.ncx -> src/epubtpl/lightnovel/tpl/EPUB/lightnovel.ncx
    	src/epub_templates/lightnovel/tpl/EPUB/lightnovel.opf -> src/epubtpl/lightnovel/tpl/EPUB/lightnovel.opf
    	src/epub_templates/lightnovel/tpl/EPUB/nav.html -> src/epubtpl/lightnovel/tpl/EPUB/nav.html
    	src/epub_templates/lightnovel/tpl/EPUB/sections-nav-template.html -> src/epubtpl/lightnovel/tpl/EPUB/sections-nav-template.html
    	src/epub_templates/lightnovel/tpl/EPUB/sections-ncx-template.xml -> src/epubtpl/lightnovel/tpl/EPUB/sections-ncx-template.xml
    	src/epub_templates/lightnovel/tpl/EPUB/sections-opf-manifest-template.xml -> src/epubtpl/lightnovel/tpl/EPUB/sections-opf-manifest-template.xml
    	src/epub_templates/lightnovel/tpl/EPUB/sections-opf-spine-template.xml -> src/epubtpl/lightnovel/tpl/EPUB/sections-opf-spine-template.xml
    	src/epub_templates/lightnovel/tpl/META-INF/container.xml -> src/epubtpl/lightnovel/tpl/META-INF/container.xml
    
    [M ] modified:
    
    	src/template.ts
    
    [A ] new file:
    
    	src/epubtpl-lib/index.ts
    	src/epubtpl-lib/postcss.ts
    	src/epubtpl-lib/zip.ts
    	src/epubtpl/from_idpf_epub3/index.ts
    	src/epubtpl/lightnovel/tpl/mimetype
    
    [D ] deleted:
    
    	src/epub_templates/from_idpf_epub3/index.ts
    	src/zip.ts
    
    [ M] modified - Changes not staged for commit:
    
    	tsconfig.json

---
+++ src/epubtpl-lib/postcss.ts
@@ -0,0 +1,29 @@
+/**
+ * Created by user on 2017/12/15/015.
+ */
+
+import * as postcss from 'postcss';
+import * as autoprefixer from 'autoprefixer';
+import * as postcss_epub from 'postcss-epub';
+
+export { postcss, autoprefixer, postcss_epub }
+
+export async function compileCss(css)
+{
+	let result = await
+		postcss([
+			postcss_epub,
+			autoprefixer({
+				add: true,
+				remove: false,
+				flexbox: false,
+			})
+		])
+			.process(css)
+	;
+
+	return result.css;
+}
+
+// @ts-ignore
+export default exports;
