42c39ea7c8a8c303c5fe81f7cf4d07a8f8f6ce71
bluelovers <codelovers@users.sourceforge.net>
2017-12-15 21:39:53 +0800

    . (Step: 1)
    Step: 1
    
    [ M] modified - Changes not staged for commit:
    
    	src/epubtpl-lib/zip.ts
    	src/epubtpl/lightnovel/index.ts

--- src/epubtpl-lib/zip.ts
+++ src/epubtpl-lib/zip.ts
@@ -4,48 +4,71 @@
 
 import * as JSZip from 'jszip';
 import * as JSZipUtils from 'jszip-utils';
+import * as util from 'util';
 import { compileTpl } from './handlebar-helpers';
 import { IEpubConfig } from '../var';
+import * as path from 'path';
+import * as Promise from 'bluebird';
+import { EpubMaker } from '../index';
 
 export { JSZip, JSZipUtils }
 
+JSZipUtils.getBinaryContentAsync = util.promisify(JSZipUtils.getBinaryContent);
+
 export function addMimetype(zip: JSZip, epubConfig: IEpubConfig, options)
 {
-	zip.file('mimetype', options.templates.mimetype);
+	return zip.file('mimetype', options.templates.mimetype);
 }
 
-export function addContainerInfo(zip, epubConfig, options)
+export function addContainerInfo(zip: JSZip, epubConfig: IEpubConfig, options)
 {
-	zip.folder('META-INF').file('container.xml', compileTpl(options.templates.container, epubConfig));
+	return zip.folder('META-INF').file('container.xml', compileTpl(options.templates.container, epubConfig));
 }
 
-export async function addCover(zip, epubConfig, options)
+export function addFiles(zip: JSZip, epubConfig: IEpubConfig, options)
+{
+	return Promise.map(epubConfig.additionalFiles, function (file)
+	{
+		return JSZipUtils.getBinaryContentAsync(file.url)
+			.then(function (data)
+			{
+				return zip
+					.folder('EPUB')
+					.folder(file.folder)
+					.file(file.filename, data, { binary: true })
+					;
+			})
+		;
+	});
+}
+
+export async function addCover(zip: JSZip, epubConfig: IEpubConfig, options)
 {
 	if (epubConfig.coverUrl)
 	{
-		return new Promise(function (resolve, reject)
-		{
-			JSZipUtils.getBinaryContent(epubConfig.coverUrl, function (err, data)
+		return JSZipUtils.getBinaryContentAsync(epubConfig.coverUrl)
+			.then(function (data)
 			{
-				if (!err)
-				{
-					let ext = epubConfig.coverUrl.substr(epubConfig.coverUrl.lastIndexOf('.') + 1);
+				let ext = path.extname(epubConfig.coverUrl);
 
-					zip.folder('EPUB')
-						//.folder('images')
-						.file(epubConfig.slug + '-cover.' + ext, data, { binary: true });
-
-					resolve(true);
-				}
-				else
-				{
-					reject(err);
-				}
-			});
-		});
+				return zip.folder('EPUB')
+					//.folder('images')
+					.file(epubConfig.slug + '-cover.' + ext, data, { binary: true });
+			})
+		;
 	}
 
 	return false;
+}
+
+export async function addSubSections(zip: JSZip, section: EpubMaker.Section, cb, epubConfig?: IEpubConfig, options?)
+{
+	await cb(zip, section, epubConfig, options);
+
+	return Promise.map(section.subSections, function (subSection: EpubMaker.Section)
+	{
+		return addSubSections(zip, subSection, cb, epubConfig, options);
+	});
 }
 
 // @ts-ignore
