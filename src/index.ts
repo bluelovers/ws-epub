22b4eba67063883ccfd649a2bf35599e7530fa4a
bluelovers <codelovers@users.sourceforge.net>
2017-12-17 15:50:27 +0800

    . (Step: 3)
    Step: 3
    
    [A ] new file:
    
    	src/plugin/file-saver.ts
    
    [ M] modified - Changes not staged for commit:
    
    	package.json
    	src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as slugify from 'slugify';
-//import './epub_templates/handlebar-helpers';
-import { saveAs } from 'file-saver';
+
 import * as moment from 'moment';
 import { templateManagers } from './template';
 import * as shortid from 'shortid';
@@ -259,37 +258,6 @@ export class EpubMaker
 			let content = await epubZip.generateAsync(generateOptions);
 
 			return content;
-		});
-	}
-
-	/**
-	 * for web
-	 *
-	 * @param callback
-	 * @param options
-	 * @returns {Promise<Blob>}
-	 */
-	downloadEpub(callback, options?): Promise<Blob>
-	{
-		options = Object.assign({
-			type: 'blob',
-			useTitle: false,
-		}, options);
-
-		let self = this;
-
-		return this.makeEpub<Blob>(options).then(async function (epubZipContent: Blob)
-		{
-			let filename = self.getFilename(options.useTitle);
-
-			console.debug('saving "' + filename + '"...');
-			if (callback && typeof(callback) === 'function')
-			{
-				await callback(epubZipContent, filename);
-			}
-			saveAs(epubZipContent, filename);
-
-			return epubZipContent;
 		});
 	}
 }
