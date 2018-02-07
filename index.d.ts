028706743f9441d15f1e7dfa79f20b65a3f5ae51
bluelovers <codelovers@users.sourceforge.net>
2018-02-07 14:04:34 +0800

    .

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 export declare const IDKEY = "epub";
 export interface IOptions {
     outputDir?: string;
+    cwd?: string;
+    log?: boolean;
 }
 export declare function epubExtract(srcFile: string, options?: IOptions): Promise<string>;
 export default epubExtract;
