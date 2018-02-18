1857e1c7eb5826376b9708e3fded1a4b7cdc7066
bluelovers <codelovers@users.sourceforge.net>
2018-02-19 05:27:17 +0800

    .

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by user on 2017/12/12/012.
+ */
+export * from './src';
+import { EpubMaker } from './src';
+export default EpubMaker;
---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by user on 2017/12/12/012.
+ */
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./src"));
+const src_1 = require("./src");
+exports.default = src_1.EpubMaker;
