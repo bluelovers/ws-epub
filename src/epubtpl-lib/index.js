e65c00d3019fe4e01fabfa6fe071d861b23e6aee
bluelovers <codelovers@users.sourceforge.net>
2018-12-06 17:02:05 +0800

    改用 html-minifier

--- src/epubtpl-lib/index.js
+++ src/epubtpl-lib/index.js
@@ -4,21 +4,54 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const js_beautify_1 = require("js-beautify");
+const html_minifier_1 = require("html-minifier");
 function formatHTML(htmlstr, skipFormatting) {
-    /*jslint camelcase:false*/
-    return (skipFormatting || typeof js_beautify_1.html_beautify === 'undefined') ? htmlstr : js_beautify_1.html_beautify(htmlstr, {
-        'end_with_newline': false,
-        'indent_char': '\t',
-        'indent_inner_html': true,
-        'indent_size': '1',
-        'preserve_newlines': false,
-        'wrap_line_length': '0',
-        'unformatted': [],
-        'selector_separator_newline': false,
-        'newline_between_rules': true
-    });
-    /*jslint camelcase:true*/
+    if (skipFormatting || typeof js_beautify_1.html_beautify === 'undefined') {
+        return htmlstr;
+    }
+    return htmlminify(htmlstr)
+        //.replace(/<item id="" href="image\/" media-type="" \/>/ig, '')
+        .replace(/^\n+|\n+$/, '\n');
 }
 exports.formatHTML = formatHTML;
+function htmlminify(html, options = {}) {
+    options = Object.assign({
+        collapseWhitespace: true,
+        preserveLineBreaks: true,
+        conservativeCollapse: true,
+        caseSensitive: true,
+        keepClosingSlash: true,
+        ignoreCustomFragments: [
+            /\<\/[ \t]*meta\>/i,
+        ],
+    }, options);
+    try {
+        let ret = html_minifier_1.minify(html, options);
+        return ret;
+    }
+    catch (e) {
+        try {
+            let ret = js_beautify_1.html_beautify(html, {
+                end_with_newline: true,
+                indent_char: '',
+                indent_inner_html: false,
+                indent_size: 0,
+                indent_level: 0,
+                max_preserve_newlines: 1,
+                preserve_newlines: true,
+                wrap_line_length: 0,
+                unformatted: [],
+                selector_separator_newline: false,
+                newline_between_rules: true
+            });
+            return ret;
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+    return html;
+}
+exports.htmlminify = htmlminify;
 const self = require("./index");
 exports.default = self;
--- src/epubtpl-lib/index.ts
+++ src/epubtpl-lib/index.ts
@@ -3,22 +3,68 @@
  */
 
 import { html_beautify } from 'js-beautify';
+import { minify, Options as IMinifyOptions } from 'html-minifier';
 
 export function formatHTML(htmlstr, skipFormatting?: boolean): string
 {
-	/*jslint camelcase:false*/
-	return (skipFormatting || typeof html_beautify === 'undefined') ? htmlstr : html_beautify(htmlstr, {
-		'end_with_newline': false,
-		'indent_char': '\t',
-		'indent_inner_html': true,
-		'indent_size': '1',
-		'preserve_newlines': false,
-		'wrap_line_length': '0',
-		'unformatted': [],
-		'selector_separator_newline': false,
-		'newline_between_rules': true
-	});
-	/*jslint camelcase:true*/
+	if (skipFormatting || typeof html_beautify === 'undefined')
+	{
+		return htmlstr;
+	}
+
+	return htmlminify(htmlstr)
+		//.replace(/<item id="" href="image\/" media-type="" \/>/ig, '')
+		.replace(/^\n+|\n+$/, '\n')
+	;
+}
+
+export function htmlminify(html: string, options: IMinifyOptions = {})
+{
+	options = Object.assign({
+		collapseWhitespace: true,
+		preserveLineBreaks: true,
+		conservativeCollapse: true,
+		caseSensitive: true,
+		keepClosingSlash: true,
+
+		ignoreCustomFragments: [
+			/\<\/[ \t]*meta\>/i,
+		],
+	}, options);
+
+	try
+	{
+		let ret = minify(html, options);
+
+		return ret;
+	}
+	catch (e)
+	{
+		try
+		{
+			let ret = html_beautify(html, {
+				end_with_newline: true,
+				indent_char: '',
+				indent_inner_html: false,
+				indent_size: 0,
+				indent_level: 0,
+				max_preserve_newlines: 1,
+				preserve_newlines: true,
+				wrap_line_length: 0,
+				unformatted: [],
+				selector_separator_newline: false,
+				newline_between_rules: true
+			});
+
+			return ret;
+		}
+		catch (e)
+		{
+			console.error(e);
+		}
+	}
+
+	return html;
 }
 
 import * as self from './index';
