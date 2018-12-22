8392c3cfee6f663ec96b4e1616c040285ba09f3c
bluelovers <codelovers@users.sourceforge.net>
2018-12-23 04:39:43 +0800

    \p{Punctuation}

--- index.d.ts
+++ index.d.ts
@@ -3,17 +3,6 @@
  */
 import * as Promise from 'bluebird';
 import { IOptions, getNovelConf, makeFilename, makeOptions, defaultOptions } from './lib/txt2epub3';
-export declare function novelEpub(options: IOptions): Promise<{
-    file: string;
-    filename: string;
-    epub: import("epub-maker2").EpubMaker;
-    outputPath: string;
-    basename: string;
-    ext: string;
-    stat: {
-        volume: number;
-        chapter: number;
-    };
-}>;
+export declare function novelEpub(options: IOptions): Promise<import("./lib/txt2epub3").INovelEpubReturnInfo>;
 export { IOptions, getNovelConf, makeFilename, makeOptions, defaultOptions };
 export default novelEpub;
